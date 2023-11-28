d5e3ddfcb36820ee9ed8bb1f96d2a56498ac82c0
Gurkan Akcakaya <gurkan.akcakaya@cegeka.com>
2023-11-28 11:53:38 +0100

    FEAT - xlsx to pdf

--- invoice_pdf.py
+++ invoice_pdf.py
@@ -1,11 +1,17 @@
 from pathlib import Path
 import pandas as pd
 import glob
-
+from fpdf import FPDF
 
 
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    pdf.set_font(family="Times", size=16, style="B")
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+    pdf.output(f"pdfs/{filename}.pdf")
