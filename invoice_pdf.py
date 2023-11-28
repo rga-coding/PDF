ee1903c4cb1d89e138236ff3b8de9aa3b76d43ca
Gurkan Akcakaya <gurkan.akcakaya@cegeka.com>
2023-11-28 11:39:54 +0100

    FEAT - Read xlsx

---
+++ invoice_pdf.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import pandas as pd
+import glob
+
+
+
+filepaths = glob.glob("invoices/*.xlsx")
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    print(df)
